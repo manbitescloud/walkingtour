d42252ded6fb60c249941c64c84688fe8e4a5b8d
John Bullit <jsbullitt@gmail.com>
2026-06-25 15:22:41 -0400

    Initial Commit

---
+++ Walking Tour/Walking_TourApp.swift
@@ -0,0 +1,17 @@
+//
+//  Walking_TourApp.swift
+//  Walking Tour
+//
+//  Created by John B on 6/25/26.
+//
+
+import SwiftUI
+
+@main
+struct Walking_TourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
